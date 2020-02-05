3c995b8a445c75c2a5bc78620cf20fb549f2021e
Mario Galeno <mgaleno@solstice.com>
2020-02-04 22:09:02 -0600

    Add core-js polyfills

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
